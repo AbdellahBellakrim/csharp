a4ea79e791865edd76efba944e249531a00b2f44
AbdellahBellakrim <bellakrim2032@gmail.com>
2024-03-01 12:11:57 +0100

    add inMemGameRepository implementing the repository pattern to seperate data access logic from bussiness logic

--- GameStore.Api/EndPoints/GameEndpoints.cs
+++ GameStore.Api/EndPoints/GameEndpoints.cs
@@ -1,49 +1,19 @@
 using GameStore.Api.Entities;
+using GameStore.Api.Repositories;
+using Microsoft.Extensions.FileSystemGlobbing;
 
 namespace GameStore.Api.EndPoints;
 
 public static class GameEndpoints
 {
     const string GetGameEndpointName = "GetGame";
-
-    static List<Game> games =
-        new()
-        {
-            new Game()
-            {
-                Id = 1,
-                Name = "The Witcher 3",
-                Genre = "RPG",
-                Price = 29.99M,
-                ReleaseDate = new DateTime(2015, 5, 19),
-                ImageUrl = "https://placehold.co/100"
-            },
-            new Game()
-            {
-                Id = 2,
-                Name = "Cyberpunk 2077",
-                Genre = "RPG",
-                Price = 59.99M,
-                ReleaseDate = new DateTime(2020, 12, 10),
-                ImageUrl = "https://placehold.co/100"
-            },
-            new Game()
-            {
-                Id = 3,
-                Name = "Doom Eternal",
-                Genre = "FPS",
-                Price = 49.99M,
-                ReleaseDate = new DateTime(2020, 3, 20),
-                ImageUrl = "https://placehold.co/100"
-            }
-        };
-
     public static RouteGroupBuilder MapGamesEndpoints(this IEndpointRouteBuilder routes)
     {
         var gamesRouteGroup = routes.MapGroup("/games").WithParameterValidation();
+        inMemGamesRepository repo = new();
 
         //  Get games endpoint
-        gamesRouteGroup.MapGet("", () => games);
+        gamesRouteGroup.MapGet("", () => repo.GetAll());
 
         //  Get game by Id endpoint
         gamesRouteGroup
@@ -51,10 +21,8 @@ public static class GameEndpoints
                 "/{id}",
                 (int id) =>
                 {
-                    Game? game = games.Find(g => g.Id == id);
-                    if (game is null)
-                        return Results.NotFound();
-                    return Results.Ok(game);
+                    Game? game = repo.Get(id);
+                    return game is not null ? Results.Ok(game) : Results.NotFound();
                 }
             )
             .WithName(GetGameEndpointName);
@@ -64,9 +32,7 @@ public static class GameEndpoints
             "/",
             (Game game) =>
             {
-                game.Id = games.Max(game => game.Id) + 1;
-                games.Add(game);
-
+                repo.Create(game);
                 return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
             }
         );
@@ -76,7 +42,7 @@ public static class GameEndpoints
             "/{id}",
             (int id, Game updatedGame) =>
             {
-                Game? existingGame = games.Find(g => g.Id == id);
+                Game? existingGame = repo.Get(id);
                 if (existingGame is null)
                     return Results.NotFound();
                 existingGame.Name = updatedGame.Name;
@@ -84,6 +50,8 @@ public static class GameEndpoints
                 existingGame.Price = updatedGame.Price;
                 existingGame.ReleaseDate = updatedGame.ReleaseDate;
                 existingGame.ImageUrl = updatedGame.ImageUrl;
+
+                repo.Update(existingGame);
                 return Results.NoContent();
             }
         );
@@ -93,9 +61,9 @@ public static class GameEndpoints
             "/{id}",
             (int id) =>
             {
-                Game? game = games.Find(g => g.Id == id);
+                Game? game = repo.Get(id);
                 if (game is not null)
-                    games.Remove(game);
+                    repo.Delete(id);
                 return Results.NoContent();
             }
         );
---
+++ GameStore.Api/Repositories/inMemGameRepository.cs
@@ -0,0 +1,66 @@
+using GameStore.Api.Entities;
+
+namespace GameStore.Api.Repositories;
+
+public class inMemGamesRepository
+{
+    private readonly List<Game> games =
+    new()
+    {
+            new Game()
+            {
+                Id = 1,
+                Name = "The Witcher 3",
+                Genre = "RPG",
+                Price = 29.99M,
+                ReleaseDate = new DateTime(2015, 5, 19),
+                ImageUrl = "https://placehold.co/100"
+            },
+            new Game()
+            {
+                Id = 2,
+                Name = "Cyberpunk 2077",
+                Genre = "RPG",
+                Price = 59.99M,
+                ReleaseDate = new DateTime(2020, 12, 10),
+                ImageUrl = "https://placehold.co/100"
+            },
+            new Game()
+            {
+                Id = 3,
+                Name = "Doom Eternal",
+                Genre = "FPS",
+                Price = 49.99M,
+                ReleaseDate = new DateTime(2020, 3, 20),
+                ImageUrl = "https://placehold.co/100"
+            }
+    };
+
+    public IEnumerable<Game> GetAll()
+    {
+        return games;
+    }
+
+    public Game? Get(int Id)
+    {
+        return games.Find(game => game.Id == Id);
+    }
+
+    public void Create(Game game)
+    {
+        game.Id = games.Max(game => game.Id) + 1;
+        games.Add(game);
+    }
+
+    public void Update(Game updatedGame)
+    {
+        var index = games.FindLastIndex(game => game.Id == updatedGame.Id);
+        games[index] = updatedGame;
+    }
+
+    public void Delete(int Id)
+    {
+        var index = games.FindIndex(game => game.Id == Id);
+        games.RemoveAt(index);
+    }
+}
